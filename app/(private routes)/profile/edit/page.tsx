390d58e407794fa915e72eab99722154d1cf2394
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-07-21 11:57:38 +0300

    35

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import css from "./ProfilePage.module.css";
+import css from "./EditProfile.module.css";
 import { getMe, updateMe } from "../../../../lib/api/clientApi";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
